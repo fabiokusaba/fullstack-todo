fd82dfeddc974f3e969d22a3d5eb1260d39792f8
Fabio Kusaba <fabinhokb44@gmail.com>
2025-08-24 20:06:54 -0300

    chore: update gitignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,31 @@
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+{
+    var myTodo = new Todo()
+    {
+        Title = "test",
+        Description = "test",
+        Id = Guid.NewGuid().ToString(),
+        Isdone = false,
+        Priority = 5
+    };
+    dbContext.Todos.Add(myTodo);
+    dbContext.SaveChanges();
+    var objects = dbContext.Todos.ToList();
+    return objects;
+});
 
 app.Run();
