d88068d180e6f238afa6573841ba4698c9a83830
Fabio Kusaba <fabinhokb44@gmail.com>
2025-09-03 21:10:34 -0300

    feat: add TodoClient

--- client/src/baseUrl.ts
+++ client/src/baseUrl.ts
@@ -1,6 +1,9 @@
+import { TodoClient } from "./generated-ts-client"
+
 const isProduction = import.meta.env.PROD
 
 const prod = ''
 const dev = 'http://localhost:5036'
 
 export const finalUrl = isProduction ? prod : dev
+export const todoClient = new TodoClient(finalUrl)
