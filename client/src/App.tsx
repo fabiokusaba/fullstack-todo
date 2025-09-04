a0c3edc830a209e3afe5671fa9cd0157bab68495
Fabio Kusaba <fabinhokb44@gmail.com>
2025-09-03 21:34:33 -0300

    feat: add create and list todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { todoClient } from './baseUrl'
+import type { CreateTodoDto, Todo } from './generated-ts-client'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [myForm, setMyForm] = useState<CreateTodoDto>({
+    title: '',
+    description: '',
+    priority: 0
+  })
+
+  useEffect(() => {
+    todoClient.getAllTodos().then(r => {
+      setTodos(r)
+    })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <input 
+      value={myForm.title} 
+      onChange={e => setMyForm({...myForm, title: e.target.value})} 
+      placeholder='your interesting title' 
+    />
+
+    <input 
+      value={myForm.description} 
+      onChange={e => setMyForm({...myForm, description: e.target.value})} 
+      placeholder='your interesting description' 
+    />
+
+    <input 
+      value={myForm.priority} 
+      onChange={e => setMyForm({...myForm, priority: Number.parseInt(e.target.value)})} 
+      type='number' 
+      placeholder='your interesting priority' 
+    />
+
+    <button onClick={() => {
+      todoClient.createTodo(myForm).then(result => {
+        console.log('todo was created successfully')
+        
+        setTodos([...todos, result])
+
+        setMyForm({
+          title: '',
+          description: '',
+          priority: 0
+        })
+      })
+    }}>Create a new todo</button>
+
+    < hr />
+
+    {
+      todos.map(t => {
+        return <div key={t.id}>
+          {JSON.stringify(t)}
+        </div>
+      })
+    }
     </>
   )
 }
